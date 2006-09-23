a2b22ab7f3e494fbf017b3ce3179961d751503ae
Filip Wasilewski <en@ig.ma>
2006-09-23 13:43:29 +0000

    release details version set to 0.1.5
    --HG--
    extra : convert_revision : svn%3A993dc4b6-72fc-0310-9b3e-91fa30ebc9a8/pywt/trunk%4049

--- pywt/release_details.py
+++ pywt/release_details.py
@@ -3,12 +3,12 @@
 # Release details for package
 
 name         = "PyWavelets"
-version      = "0.1.4"
-#revision     = "$Revision$"[11:-2]
+version      = "0.1.5"
+#revision     = "$Revision$".split()[1]
 author       = "Filip Wasilewski"
 author_email = "filipwasilewski@gmail.com"
 url          = "http://www.pybytes.com/pywavelets/"
-download_url = ""
+download_url = "http://cheeseshop.python.org/pypi/PyWavelets"
 license      = "MIT"
 description  = "PyWavelets, wavelet transform module."
 keywords     = ['numpy', 'wavelets', 'scientific']
@@ -16,9 +16,9 @@ platforms    = ['Linux', 'Mac OSX', 'Windows XP/2000/NT']
 svn          = "http://wavelets.scipy.org/svn/multiresolution/pywt/trunk"
 
 long_description = \
-"""PyWavelets is a Python module that can perform discrete forward and
-inverse wavelet transform, stationary wavelet transform and wavelet
-packets signal decomposition and reconstruction.
+"""PyWavelets is a Python module that can perform 1D and 2D Discrete
+Forward and Inverse Wavelet Transform, Stationary Wavelet Transform
+and Wavelet Packets signal decomposition and reconstruction.
 """
 
 classifiers = [
