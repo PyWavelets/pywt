9a5a7de9b305a6454e44001291f45cad6185f52e
Filip Wasilewski <en@ig.ma>
2007-09-11 22:36:33 +0000

    Added regression tests
    --HG--
    extra : convert_revision : svn%3A993dc4b6-72fc-0310-9b3e-91fa30ebc9a8/pywt/trunk%4079

---
+++ tests/test_regression.py
@@ -0,0 +1,7 @@
+import doctest
+doctest.testfile("regression/multilevel.txt")
+doctest.testfile("regression/modes.txt")
+doctest.testfile("regression/wavelet.txt")
+doctest.testfile("regression/dwt_idwt.txt")
+doctest.testfile("regression/wp.txt")
+doctest.testfile("regression/gotchas.txt")
