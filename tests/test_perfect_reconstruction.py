629b60219bb24121088361bf83db41f9adbe8996
Filip Wasilewski <en@ig.ma>
2007-10-30 00:31:32 +0000

    Test perfect reconstruction for both float32 and float64 input data types
    --HG--
    extra : convert_revision : svn%3A993dc4b6-72fc-0310-9b3e-91fa30ebc9a8/pywt/trunk%4092

--- tests/test_perfect_reconstruction.py
+++ tests/test_perfect_reconstruction.py
@@ -3,11 +3,11 @@
 """
 Verify DWT perfect reconstruction.
 """
-    
+
 import math
 import numpy
 import pywt
-from numpy import asarray, float64
+from numpy import asarray, float64, float32
 
 def mse(ar1, ar2):
     """Mean squared error"""
@@ -21,28 +21,27 @@ def rms(ar1, ar2):
     """Root mean squared error"""
     return math.sqrt(mse(ar1, ar2))
 
-def test_perfect_reconstruction(families, wavelets, modes, epsilon):
-    print "Testing perfect reconstruction".upper()
-    for pmode, mmode in modes:
-        for wavelet in wavelets:
+def test_perfect_reconstruction(families, wavelets, modes, epsilon, dtype):
+    for wavelet in wavelets:
+        for pmode, mmode in modes:
             print "Wavelet: %-8s Mode: %s" % (wavelet, pmode),
-        
+
             w = pywt.Wavelet(wavelet)
             data_size = range(2, 40) + [100, 200, 500, 1000, 2000, 10000, 50000, 100000]
-            
+
             ok, over = 0, 0
             for N in data_size:
-                data = numpy.random.random(N)
-                
+                data = numpy.asarray(numpy.random.random(N), dtype)
+
                 # compute dwt coefficients
                 pa, pd = pywt.dwt(data, wavelet, pmode)
-                
+
                 # compute reconstruction
                 rec = pywt.idwt(pa, pd, wavelet, pmode)
 
                 if len(data) % 2:
                     rec = rec[:len(data)]
-                    
+
                 rms_rec = rms(data, rec)
                 if rms_rec > epsilon:
                     if not over:
@@ -55,13 +54,16 @@ def test_perfect_reconstruction(families, wavelets, modes, epsilon):
                 print "- RMSE for all %d cases was under %s" % (len(data_size), epsilon)
 
 if __name__ == '__main__':
-    
+
     families = ('db', 'sym', 'coif', 'bior', 'rbio')
     wavelets = sum([pywt.wavelist(name) for name in families], [])
     # list of mode names in pywt and matalb
     modes = [('zpd', 'zpd'), ('cpd', 'sp0'), ('sym', 'sym'),
-             ('ppd', 'ppd'), ('sp1', 'sp1'), ('per', 'per')] 
-    # max RMSE
-    epsilon = 1.0e-10
-    
-    test_perfect_reconstruction(families, wavelets, modes, epsilon)
+             ('ppd', 'ppd'), ('sp1', 'sp1'), ('per', 'per')]
+
+    print "Testing perfect reconstruction".upper()
+    for dtype, name, epsilon in [(float32, "float32", 1.0e-7), (float64, "float64", 0.5e-10)][::-1]:
+        print "#"*80 + "\nPrecision: %s, max RMSE: %s\n" % (name, epsilon) + "#"*80 + "\n"
+        test_perfect_reconstruction(families, wavelets, modes, epsilon=epsilon, dtype=dtype)
+        print
+
