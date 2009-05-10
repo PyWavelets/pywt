c2504dea86a0585cc91a396384323bfedca3a324
Filip Wasilewski <en@ig.ma>
2009-05-10 21:49:35 +0000

    Added other functions module.
    --HG--
    extra : convert_revision : svn%3A993dc4b6-72fc-0310-9b3e-91fa30ebc9a8/pywt/trunk%40136

--- pywt/__init__.py
+++ pywt/__init__.py
@@ -14,6 +14,7 @@ from _pywt import *
 from multilevel import *
 from multidim import *
 from wavelet_packets import *
+from functions import *
 import thresholding
 
 from release_details import version as __version__, author as __author__, license as __license__
@@ -22,6 +23,7 @@ __all__ += _pywt.__all__
 __all__ += wavelet_packets.__all__
 __all__ += multilevel.__all__
 __all__ += multidim.__all__
+__all__ += functions.__all__
 __all__ += ['thresholding']
 
 del multilevel, multidim, wavelet_packets
---
+++ pywt/functions.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2006-2008 Filip Wasilewski <filip.wasilewski@gmail.com>
+# See COPYING for license details.
+
+# $Id$
+
+"""
+Other wavelet related functions.
+"""
+
+__all__ = ["intwave", "centfrq", "scal2frq", "qmf", "orthfilt"]
+
+from math import sqrt
+
+from _pywt import Wavelet
+#from cwt import CWavelet
+from helpers import wavelet_for_name
+
+from numerix import asarray, array, float64
+from numerix import integrate
+from numerix import argmax, mean
+from numerix import fft
+
+WAVELET_CLASSES = (Wavelet)
+
+
+def intwave(wavelet, precision=8):
+    """
+    intwave(wavelet, precision=8) -> [int_psi, x]                   - for orthogonal wavelets
+    intwave(wavelet, precision=8) -> [int_psi_d, int_psi_r, x]      - for other wavelets
+    intwave((function_approx, x), precision=8) -> [int_function, x] - for (function approx., x grid) pair
+
+    Integrate *psi* wavelet function from -Inf to x using the rectangle
+    integration method.
+
+    wavelet         - Wavelet to integrate (Wavelet object, wavelet name string
+                      or (wavelet function approx., x grid) pair)
+
+    precision = 8   - Precision that will be used for wavelet function
+                      approximation computed with the wavefun(level=precision)
+                      Wavelet's method.
+
+    (function_approx, x)   - Function to integrate on the x grid. Used instead
+                             of Wavelet object to allow custom wavelet functions.
+    """
+
+    if isinstance(wavelet, tuple):
+        psi, x = asarray(wavelet[0]), asarray(wavelet[1])
+        step = x[1] - x[0]
+        return integrate(psi, step), x
+
+    else:
+        if not isinstance(wavelet, WAVELET_CLASSES):
+            wavelet = wavelet_for_name(wavelet)
+
+        functions_approximations = wavelet.wavefun(precision)
+        if len(functions_approximations) == 2:      # continuous wavelet
+            psi, x = functions_approximations
+            step = x[1] - x[0]
+            return integrate(psi, step), x
+        elif len(functions_approximations) == 3:    # orthogonal wavelet
+            phi, psi, x = functions_approximations
+            step = x[1] - x[0]
+            return integrate(psi, step), x
+        else: # biorthogonal wavelet
+            phi_d, psi_d, phi_r, psi_r, x = functions_approximations
+            step = x[1] - x[0]
+            return integrate(psi_d, step), integrate(psi_r, step), x
+
+
+def centfrq(wavelet, precision=8):
+    """
+    centfrq(wavelet, precision=8) -> float                  - for orthogonal wavelets
+    centfrq((function_aprox, x), precision=8) -> float      - for (function approx., x grid) pair
+
+    Computes the central frequency of the *psi* wavelet function.
+
+    wavelet         - Wavelet (Wavelet object, wavelet name string
+                      or (wavelet function approx., x grid) pair)
+    precision = 8   - Precision that will be used for wavelet function
+                      approximation computed with the wavefun(level=precision)
+                      Wavelet's method.
+
+    (function_approx, xgrid)   - Function defined on xgrid. Used instead
+                                 of Wavelet object to allow custom wavelet functions.
+    """
+
+    if isinstance(wavelet, tuple):
+        psi, x = asarray(wavelet[0]), asarray(wavelet[1])
+    else:
+        if not isinstance(wavelet, WAVELET_CLASSES):
+            wavelet = wavelet_for_name(wavelet)
+        functions_approximations = wavelet.wavefun(precision)
+
+        if len(functions_approximations) == 2:
+            psi, x = functions_approximations
+        else:
+            psi, x = functions_approximations[1], functions_approximations[-1] # (psi, x) for (phi, psi, x) and (psi_d, x) for (phi_d, psi_d, phi_r, psi_r, x)
+
+    domain = float(x[-1] - x[0])
+    assert domain > 0
+
+    index = argmax(abs(fft(psi)[1:]))+2
+    if index > len(psi)/2:
+        index = len(psi)-index+2
+
+    return 1.0/(domain/(index-1))
+
+
+def scal2frq(wavelet, scale, delta, precision=8):
+    """
+    scal2frq(wavelet, scale, delta, precision=8) -> float   - for orthogonal wavelets
+    scal2frq(wavelet, scale, delta, precision=8) -> float   - for (function approx., x grid) pair
+
+    wavelet
+    scale
+    delta   - sampling
+    """
+    return centfrq(wavelet, precision=precision)/(scale*delta)
+
+
+def qmf(filter):
+    filter = array(filter)[::-1]
+    filter[1::2] = -filter[1::2]
+    return filter
+
+
+def orthfilt(scaling_filter):
+    assert len(scaling_filter) % 2 == 0
+
+    scaling_filter = asarray(scaling_filter, dtype=float64)
+
+    rec_lo = sqrt(2) * scaling_filter / sum(scaling_filter)
+    dec_lo = rec_lo[::-1]
+
+    rec_hi = qmf(rec_lo)
+    dec_hi = rec_hi[::-1]
+
+    return (dec_lo, dec_hi, rec_lo, rec_hi)
