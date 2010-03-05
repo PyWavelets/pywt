024d53af21fbc0df61d0b102acf580e391d10efe
Filip Wasilewski <en@ig.ma>
2010-03-05 09:18:46 +0000

    Minor fix to demo code.
    --HG--
    extra : convert_revision : svn%3A993dc4b6-72fc-0310-9b3e-91fa30ebc9a8/pywt/trunk%40141

--- demo/dwt_multidim.py
+++ demo/dwt_multidim.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
+import pprint
 import numpy
 import pywt
 
-if __name__ == '__main__':
-   import pprint
-   data = numpy.ones((4, 4, 4, 4))   # 4D array
-   result = pywt.dwtn(data , 'db1')) # sixteen 4D coefficient arrays
-   pprint.pprint(result)
+data = numpy.ones((4, 4, 4, 4))   # 4D array
+result = pywt.dwtn(data , 'db1') # sixteen 4D coefficient arrays
+pprint.pprint(result)
