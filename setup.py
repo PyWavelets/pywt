b481273a2a1cb9d6c61c3969553cde4092045cfe
Filip Wasilewski <en@ig.ma>
2012-07-14 01:28:27 +0200

    Using setuptools if available, failing back to distutils otherwise.

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@
 #-*- coding: utf-8 -*-
 
 import os, sys
-from distutils.core import setup, Extension
+
+try:
+    from setuptools import setup, Extension
+    has_setuptools = True
+except ImportError:
+    from distutils.core import setup, Extension
+    has_setuptools = False
 
 from util import commands
 
@@ -27,6 +33,10 @@ cmdclass={
     "sdist": commands.SdistCommand,
     "clean_build": commands.CleanCommand,
 }
+
+setup_args = {}
+if has_setuptools:
+    setup_args["zip_safe"] = False
 
 setup(
     name="PyWavelets",
@@ -64,5 +74,5 @@ setup(
     packages=packages,
     package_dir=package_dir,
     cmdclass=cmdclass,
-    zip_safe=False,
+    **setup_args
 )
