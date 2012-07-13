75d00cec8f360707e00584b9bd07d4c17473ff8e
Filip Wasilewski <en@ig.ma>
2012-07-14 01:19:02 +0200

    Converted doctests to unittest.
    Run all doctests in a single test suite.

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python
 
-import os
-import glob
-import doctest
-
-docs_base = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, "doc", "source"))
-files = glob.glob(os.path.join(docs_base, "regression", "*.rst"))
-
-assert files
-
-for path in files:
-    print "testing %s" % path
-    doctest.testfile(path)
+# tests moved to test_doc.py
