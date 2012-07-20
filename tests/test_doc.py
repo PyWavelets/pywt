92541511bce00b3f3a63761e0afa4b575b778619
Filip Wasilewski <en@ig.ma>
2012-07-21 00:12:26 +0200

    Simplify doc source glob

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -10,8 +10,7 @@ docs_base = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.path.pardir, "doc", "source"))
 
 files = glob.glob(os.path.join(docs_base, "*.rst"))\
-        + glob.glob(os.path.join(docs_base, "ref", "*.rst"))\
-        + glob.glob(os.path.join(docs_base, "regression", "*.rst"))
+        + glob.glob(os.path.join(docs_base, "*", "*.rst"))
 
 assert files
 
