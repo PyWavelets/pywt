ee4ee344068120846cf9bc79ce3d83d6381f69da
François Boulogne <fboulogne sciunto org>
2013-07-26 19:45:33 +0200

    DOC: add docstring for the thresholding module.

--- src/pywt/thresholding.py
+++ src/pywt/thresholding.py
@@ -3,7 +3,10 @@
 # Copyright (c) 2006-2012 Filip Wasilewski <http://en.ig.ma/>
 # See COPYING for license details.
 
-"""Thresholding routines"""
+"""
+The thresholding helper module implements the most popular signal thresholding
+functions.
+"""
 
 from __future__ import division, print_function, absolute_import
 
@@ -13,6 +16,32 @@ import numpy as np
 
 
 def soft(data, value, substitute=0):
+    """
+    Soft thresholding.
+
+    Parameters
+    ----------
+    data : array_like
+        Numeric data.
+    value : scalar
+        Thresholding value.
+    substitute : double, optional
+        Substitute value (default: 0).
+
+    Returns
+    -------
+    soft : array
+        Result.
+
+    Examples
+    --------
+    >>> from pywt import thresholding
+    >>> data = np.linspace(1, 4, 7)
+    >>> data
+    array([ 1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ])
+    >>> thresholding.soft(data, 2)
+    array([ 0. ,  0. ,  0. ,  0.5,  1. ,  1.5,  2. ])
+    """
     mvalue = -value
 
     cond_less = np.less(data, value)
@@ -26,6 +55,37 @@ def soft(data, value, substitute=0):
 
 
 def hard(data, value, substitute=0):
+    """
+    Hard thresholding.
+
+    Replace all data values with substitute where their
+    absolute value is less than the value param.
+    Data values with absolute value greater or equal to the thresholding value
+    stay untouched.
+
+    Parameters
+    ----------
+    data : array_like
+        Numeric data.
+    value : scalar
+        Thresholding value.
+    substitute : double, optional
+        Substitute value (default: 0).
+
+    Returns
+    -------
+    hard : array
+        Result.
+
+    Examples
+    --------
+    >>> from pywt import thresholding
+    >>> data = np.linspace(1, 4, 7)
+    >>> data
+    array([ 1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ])
+    >>> thresholding.hard(data, 2)
+    array([ 0. ,  0. ,  2. ,  2.5,  3. ,  3.5,  4. ])
+    """
     mvalue = -value
 
     cond = np.less(data, value)
@@ -35,10 +95,64 @@ def hard(data, value, substitute=0):
 
 
 def greater(data, value, substitute=0):
+    """
+    Replace data with substitute where data is below the thresholding value.
+    Greater data values pass untouched.
+
+    Parameters
+    ----------
+    data : array_like
+        Numeric data.
+    value : scalar
+        Thresholding value.
+    substitute : double, optional
+        Substitute value (default: 0).
+
+    Returns
+    -------
+    greater : array
+        Result.
+
+    Examples
+    --------
+    >>> from pywt import thresholding
+    >>> data = np.linspace(1, 4, 7)
+    >>> data
+    array([ 1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ])
+    >>> thresholding.greater(data, 2)
+    array([ 0. ,  0. ,  2. ,  2.5,  3. ,  3.5,  4. ])
+    """
     return np.where(np.less(data, value), substitute, data)
 
 
 def less(data, value, substitute=0):
+    """
+    Replace data with substitute where data is above the thresholding value.
+    Less data values pass untouched.
+
+    Parameters
+    ----------
+    data : array_like
+        Numeric data.
+    value : scalar
+        Thresholding value.
+    substitute : double, optional
+        Substitute value (default: 0).
+
+    Returns
+    -------
+    less : array
+        Result.
+
+    Examples
+    --------
+    >>> from pywt import thresholding
+    >>> data = np.linspace(1, 4, 7)
+    >>> data
+    array([ 1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ])
+    >>> thresholding.less(data, 2)
+    array([ 1. ,  1.5,  2. ,  0. ,  0. ,  0. ,  0. ])
+    """
     return np.where(np.greater(data, value), substitute, data)
 
 
