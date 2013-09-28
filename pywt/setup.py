cd96181af30ec56e8ad83fda7f0f8ef85d6b824e
Ralf Gommers <ralf.gommers@googlemail.com>
2013-09-28 21:37:59 +0200

    BLD: more fixes to make new repo layout work.

--- pywt/setup.py
+++ pywt/setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division, print_function, absolute_import
 
-from os.path import join
-
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
@@ -18,4 +16,12 @@ def configuration(parent_package='',top_path=None):
                  "src/wavelets.c", "src/wt.c"],
         include_dirs=["src", np.get_include()],
         define_macros=[("PY_EXTENSION", None)],
-    )
+        )
+
+    config.make_config_py()
+    return config
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
--- pywt/tests/test_doc.py
+++ pywt/tests/test_doc.py
@@ -8,14 +8,13 @@ import unittest
 
 pdir = os.path.pardir
 docs_base = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                            pdir, pdir, pdir, "doc", "source"))
+                            pdir, pdir, "doc", "source"))
 
 files = glob.glob(os.path.join(docs_base, "*.rst"))\
         + glob.glob(os.path.join(docs_base, "*", "*.rst"))
 
-assert files
-
 suite = doctest.DocFileSuite(*files, module_relative=False, encoding="utf-8")
+
 
 if __name__ == "__main__":
     unittest.TextTestRunner().run(suite)
