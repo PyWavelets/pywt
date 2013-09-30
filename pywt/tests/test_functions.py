19713ef53af6c0f2e9aa3f1283e419fbcb30304a
François Boulogne <fboulogne sciunto org>
2013-09-30 22:22:37 +0200

    TEST: fix names

--- pywt/tests/test_functions.py
+++ pywt/tests/test_functions.py
@@ -12,24 +12,24 @@ def test_centrfreq():
     # db1 is Haar function, frequency=1
     w = pywt.Wavelet('db1')
     expected = 1
-    result = pywt.centrfreq(w, precision=12)
+    result = pywt.centfrq(w, precision=12)
     assert_almost_equal(result, expected)
 
 
-def test_scal2freq_scale():
+def test_scal2frq_scale():
     scale = 2
     delta = 1
     w = pywt.Wavelet('db1')
     expected = 1. / scale
-    result = scal2frq(w, scale, delta, precision=8)
+    result = pywt.scal2frq(w, scale, delta, precision=8)
     assert_almost_equal(result, expected)
 
-def test_scal2freq_scale():
+def test_scal2frq_delta():
     scale = 1
     delta = 2
     w = pywt.Wavelet('db1')
     expected = 1. / delta
-    result = scal2frq(w, scale, delta, precision=8)
+    result = pywt.scal2frq(w, scale, delta, precision=8)
     assert_almost_equal(result, expected)
 
 
