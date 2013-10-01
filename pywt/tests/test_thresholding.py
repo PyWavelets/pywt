d48a65cb5232381cd33a70c978d301c266ca6591
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2013-10-01 09:47:11 +0200

    Adding 2d tests

--- pywt/tests/test_thresholding.py
+++ pywt/tests/test_thresholding.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
-from numpy.testing import assert_, assert_allclose, run_module_suite
+from numpy.testing import assert_allclose, run_module_suite
 
 from pywt import thresholding
 
@@ -9,24 +9,40 @@ def test_soft():
     data = np.linspace(1, 4, 7)
     assert_allclose(thresholding.soft(data, 2),
                     np.array([0., 0., 0., 0.5, 1., 1.5, 2.]), rtol=1e-12)
+    assert_allclose(thresholding.soft([[1, 2]] * 2, 1), [[0, 1]] * 2,
+                    rtol=1e-12)
+    assert_allclose(thresholding.soft([[1, 2]] * 2, 2), [[0, 0]] * 2,
+                    rtol=1e-12)
 
 
 def test_hard():
     data = np.linspace(1, 4, 7)
     assert_allclose(thresholding.hard(data, 2),
                     np.array([0., 0., 2., 2.5, 3., 3.5, 4.]), rtol=1e-12)
+    assert_allclose(thresholding.hard([[1, 2]] * 2, 1), [[1, 2]] * 2,
+                    rtol=1e-12)
+    assert_allclose(thresholding.hard([[1, 2]] * 2, 2), [[0, 2]] * 2,
+                    rtol=1e-12)
 
 
 def test_greater():
     data = np.linspace(1, 4, 7)
     assert_allclose(thresholding.greater(data, 2),
                     np.array([0., 0., 2., 2.5, 3., 3.5, 4.]), rtol=1e-12)
+    assert_allclose(thresholding.greater([[1, 2]] * 2, 1), [[1, 2]] * 2,
+                    rtol=1e-12)
+    assert_allclose(thresholding.greater([[1, 2]] * 2, 2), [[0, 2]] * 2,
+                    rtol=1e-12)
 
 
 def test_less():
     data = np.linspace(1, 4, 7)
     assert_allclose(thresholding.less(data, 2),
                     np.array([1., 1.5, 2., 0., 0., 0., 0.]), rtol=1e-12)
+    assert_allclose(thresholding.less([[1, 2]] * 2, 1), [[1, 0]] * 2,
+                    rtol=1e-12)
+    assert_allclose(thresholding.less([[1, 2]] * 2, 2), [[1, 2]] * 2,
+                    rtol=1e-12)
 
 if __name__ == '__main__':
     run_module_suite()
