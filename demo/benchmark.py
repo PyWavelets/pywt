a541ec88a2d83e2902d20dc51b576242f7cf5a2b
Ralf Gommers <ralf.gommers@googlemail.com>
2013-10-02 10:42:48 +0200

    MAINT: demos: update benchmarks.py and dwt_signal_decomposition.py

--- demo/benchmark.py
+++ demo/benchmark.py
@@ -5,10 +5,11 @@ import gc
 import sys
 import time
 
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 import pywt
+
 
 if sys.platform == 'win32':
     clock = time.clock
@@ -25,7 +26,6 @@ wavelet_names = ['db1', 'db2', 'db3', 'db4', 'db5', 'db6', 'db7',
                  'db8', 'db9', 'db10', 'sym10', 'coif1', 'coif2',
                  'coif3', 'coif4', 'coif5']
 
-dtype = numpy.float64
 
 wavelets = [pywt.Wavelet(n) for n in wavelet_names]
 mode = pywt.MODES.zpd
@@ -33,18 +33,13 @@ mode = pywt.MODES.zpd
 times_dwt = [[] for i in range(len(wavelets))]
 times_idwt = [[] for i in range(len(wavelets))]
 
-repeat = 5
-
 for j, size in enumerate(sizes):
-    #if size > 500000:
-    #    warnings.warn("Warning, too big data size may cause page swapping.")
-
-    data = numpy.ones((size,), dtype)
-
+    data = np.ones((size,), dtype=np.float64)
     print((("%d/%d" % (j + 1, len(sizes))).rjust(6), str(size).rjust(9)))
     for i, w in enumerate(wavelets):
         min_t1, min_t2 = 9999., 9999.
-        for _ in range(repeat):
+        for _ in range(5):
+            # Repeat timing 5 times to reduce run-to-run variation
             t1 = clock()
             (a, d) = pywt.dwt(data, w, mode)
             t1 = clock() - t1
@@ -57,17 +52,21 @@ for j, size in enumerate(sizes):
 
         times_dwt[i].append(min_t1)
         times_idwt[i].append(min_t2)
+
     gc.collect()
 
+
 for j, (times, name) in enumerate([(times_dwt, 'dwt'), (times_idwt, 'idwt')]):
-    pylab.figure(j)
-    pylab.title(name)
+    fig = plt.figure(j)
+    ax = fig.add_subplot(111)
+    ax.set_title(name)
 
     for i, n in enumerate(wavelet_names):
-        pylab.loglog(sizes, times[i], label=n)
+        ax.loglog(sizes, times[i], label=n)
 
-    pylab.legend(loc='best')
-    pylab.xlabel('len(x)')
-    pylab.ylabel('time [s]')
+    ax.legend(loc='best')
+    ax.set_xlabel('len(x)')
+    ax.set_ylabel('time [s]')
 
-pylab.show()
+
+plt.show()
