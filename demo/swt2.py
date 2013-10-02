f3e8864404e28bafe582e0e1c2f480ffb21f9ffb
Ralf Gommers <ralf.gommers@googlemail.com>
2013-10-02 12:01:56 +0200

    MAINT: demo: update swt2.py demo, add figure and axis titles.

--- demo/swt2.py
+++ demo/swt2.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pylab
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 import Image  # PIL
 
 import pywt
 
+
 im = Image.open("data/aero.png").convert('L')
-arr = numpy.fromstring(im.tostring(), numpy.uint8)
+arr = np.fromstring(im.tostring(), np.uint8)
 arr.shape = (im.size[1], im.size[0])
 
-pylab.imshow(arr, interpolation="nearest", cmap=pylab.cm.gray)
+plt.imshow(arr, interpolation="nearest", cmap=plt.cm.gray)
 
+level = 0
+titles = ['Approximation', ' Horizontal detail',
+          'Vertical detail', 'Diagonal detail']
 for LL, (LH, HL, HH) in pywt.swt2(arr, 'bior1.3', level=3, start_level=0):
-    pylab.figure()
+    fig = plt.figure()
     for i, a in enumerate([LL, LH, HL, HH]):
-        pylab.subplot(2, 2, i + 1)
-        pylab.imshow(a, origin='image', interpolation="nearest",
-            cmap=pylab.cm.gray)
+        ax = fig.add_subplot(2, 2, i + 1)
+        ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+        ax.set_title(titles[i], fontsize=12)
 
-pylab.show()
+    fig.suptitle("SWT2 coefficients, level %s" % level, fontsize=14)
+    level += 1
+
+
+plt.show()
