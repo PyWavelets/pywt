3fe8ad8f1316243e1ed1b747d2b1f54b9e1a0ec3
Aaron O'Leary <aaron.oleary@gmail.com>
2014-09-05 16:19:35 +0100

    iswt: add basic code to core

--- pywt/src/_pywt.pyx
+++ pywt/src/_pywt.pyx
@@ -3,7 +3,7 @@
 
 __doc__ = """Pyrex wrapper for low-level C wavelet transform implementation."""
 __all__ = ['MODES', 'Wavelet', 'dwt', 'dwt_coeff_len', 'dwt_max_level',
-           'idwt', 'swt', 'swt_max_level', 'upcoef', 'downcoef',
+           'idwt', 'swt', 'swt_max_level', 'iswt', 'upcoef', 'downcoef',
            'wavelist', 'families']
 
 ###############################################################################
@@ -1241,6 +1241,56 @@ def keep(arr, keep_length):
         return arr[left_bound:left_bound + keep_length]
     return arr
 
+###############################################################################
+# iswt
+
+def iswt(coefficients, wavelet):
+    """
+      Input parameters:
+
+        coefficients
+          approx and detail coefficients, arranged in level value
+          exactly as output from swt:
+          e.g. [(cA1, cD1), (cA2, cD2), ..., (cAn, cDn)]
+
+        wavelet
+          Either the name of a wavelet or a Wavelet object
+
+    """
+    output = coefficients[0][0].copy() # Avoid modification of input data
+
+    #num_levels, equivalent to the decomposition level, n
+    num_levels = len(coefficients)
+    for j in range(num_levels,0,-1):
+        step_size = int(pow(2, j-1))
+        last_index = step_size
+        _, cD = coefficients[num_levels - j]
+        for first in range(last_index): # 0 to last_index - 1
+
+            # Getting the indices that we will transform
+            indices = np.arange(first, len(cD), step_size)
+
+            # select the even indices
+            even_indices = indices[0::2]
+            # select the odd indices
+            odd_indices = indices[1::2]
+
+            # perform the inverse dwt on the selected indices,
+            # making sure to use periodic boundary conditions
+            x1 = idwt(output[even_indices], cD[even_indices], wavelet,
+                      mode='per')
+            x2 = idwt(output[odd_indices], cD[odd_indices], wavelet,
+                      mode='per')
+
+            # perform a circular shift right
+            x2 = np.roll(x2, 1)
+
+            # average and insert into the correct indices
+            output[indices] = (x1 + x2)/2.
+
+    return output
+
+###############################################################################
 
 
 # Some utility functions
