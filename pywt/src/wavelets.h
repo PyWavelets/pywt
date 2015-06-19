efcb9cbc6100112de4b59d2c039d710dabef44bb
Kai Wohlfahrt <kjw53@cam.ac.uk>
2015-06-19 12:46:06 +0100

    STY: Indenting of struct members

--- pywt/src/wavelets.h
+++ pywt/src/wavelets.h
@@ -19,14 +19,14 @@ typedef enum {
 
 /* Wavelet structure holding pointers to filter arrays and property attributes */
 typedef struct {
-        double* dec_hi_double;  /* highpass decomposition */
-        double* dec_lo_double;  /* lowpass decomposition */
-        double* rec_hi_double;  /* highpass reconstruction */
-        double* rec_lo_double;  /* lowpass reconstruction */
-        float* dec_hi_float;
-        float* dec_lo_float;
-        float* rec_hi_float;
-        float* rec_lo_float;
+    double* dec_hi_double;  /* highpass decomposition */
+    double* dec_lo_double;  /* lowpass decomposition */
+    double* rec_hi_double;  /* highpass reconstruction */
+    double* rec_lo_double;  /* lowpass reconstruction */
+    float* dec_hi_float;
+    float* dec_lo_float;
+    float* rec_hi_float;
+    float* rec_lo_float;
 
     size_t dec_len;   /* length of decomposition filter */
     size_t rec_len;   /* length of reconstruction filter */
