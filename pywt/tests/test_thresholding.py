a75a332d03d2fb3059e61971c3b28172f58bb0b7
Gregory R. Lee <gregory.lee@cchmc.org>
2015-07-22 20:13:35 -0400

    TST: test negative values as well for hard and soft thresholds

--- pywt/tests/test_thresholding.py
+++ pywt/tests/test_thresholding.py
@@ -8,8 +8,11 @@ import pywt
 def test_threshold():
     # soft
     data = np.linspace(1, 4, 7)
+    soft_result = [0., 0., 0., 0.5, 1., 1.5, 2.]
     assert_allclose(pywt.threshold(data, 2, 'soft'),
-                    np.array([0., 0., 0., 0.5, 1., 1.5, 2.]), rtol=1e-12)
+                    np.array(soft_result), rtol=1e-12)
+    assert_allclose(pywt.threshold(-data, 2, 'soft'),
+                    -np.array(soft_result), rtol=1e-12)
     assert_allclose(pywt.threshold([[1, 2]] * 2, 1, 'soft'),
                     [[0, 1]] * 2, rtol=1e-12)
     assert_allclose(pywt.threshold([[1, 2]] * 2, 2, 'soft'),
@@ -17,8 +20,11 @@ def test_threshold():
 
     # hard
     data = np.linspace(1, 4, 7)
+    hard_result = [0., 0., 2., 2.5, 3., 3.5, 4.]
     assert_allclose(pywt.threshold(data, 2, 'hard'),
-                    np.array([0., 0., 2., 2.5, 3., 3.5, 4.]), rtol=1e-12)
+                    np.array(hard_result), rtol=1e-12)
+    assert_allclose(pywt.threshold(-data, 2, 'hard'),
+                    -np.array(hard_result), rtol=1e-12)
     assert_allclose(pywt.threshold([[1, 2]] * 2, 1, 'hard'),
                     [[1, 2]] * 2, rtol=1e-12)
     assert_allclose(pywt.threshold([[1, 2]] * 2, 2, 'hard'),
