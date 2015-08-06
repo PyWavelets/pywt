a5d133510118ce58ea552f04917305b487fdbdc7
Gregory R. Lee <gregory.lee@cchmc.org>
2015-08-05 20:51:47 -0400

    ENH: add complex support to dwt, idwt

--- pywt/src/_pywt.pyx
+++ pywt/src/_pywt.pyx
@@ -642,6 +642,12 @@ def dwt(object data, object wavelet, object mode='sym'):
     [-0.70710678 -0.70710678 -0.70710678]
 
     """
+    if np.iscomplexobj(data):
+        data = np.asarray(data)
+        cA_r, cD_r = dwt(data.real, wavelet, mode)
+        cA_i, cD_i = dwt(data.imag, wavelet, mode)
+        return  (cA_r + 1j*cA_i, cD_r + 1j*cD_i)
+
     # accept array_like input; make a copy to ensure a contiguous array
     dt = _check_dtype(data)
     data = np.array(data, dtype=dt)
@@ -791,30 +797,43 @@ def idwt(cA, cD, object wavelet, object mode='sym', int correct_size=0):
     """
     # accept array_like input; make a copy to ensure a contiguous array
 
-    if cA is None and cD is None:
+    has_cA = not (cA is None)
+    has_cD = not (cD is None)
+    if not (has_cA or has_cD):
         raise ValueError("At least one coefficient parameter must be "
                          "specified.")
 
-    if cA is not None:
+    # for complex inputs: compute real and imaginary separately then combine
+    if (has_cA and np.iscomplexobj(cA)) or (has_cD and np.iscomplexobj(cD)):
+        if not has_cA:
+            cD = np.asarray(cD)
+            cA = np.zeros_like(cD)
+        elif not has_cD:
+            cA = np.asarray(cA)
+            cD = np.zeros_like(cA)
+        return (idwt(cA.real, cD.real, wavelet, mode, correct_size) +
+                1j*idwt(cA.imag, cD.imag, wavelet, mode, correct_size))
+
+    if has_cA:
         dt = _check_dtype(cA)
         cA = np.array(cA, dtype=dt)
         if cA.ndim != 1:
             raise ValueError("idwt requires 1D coefficient arrays.")
-    if cD is not None:
+    if has_cD:
         dt = _check_dtype(cD)
         cD = np.array(cD, dtype=dt)
         if cD.ndim != 1:
             raise ValueError("idwt requires 1D coefficient arrays.")
 
-    if cA is not None and cD is not None:
+    if has_cA and has_cD:
         if cA.dtype != cD.dtype:
             # need to upcast to common type
             cA = cA.astype(np.float64)
             cD = cD.astype(np.float64)
-    elif cA is None:
-        cA = np.zeros(cD.shape, dtype=cD.dtype)
-    elif cD is None:
-        cD = np.zeros(cA.shape, dtype=cA.dtype)
+    elif has_cD:
+        cA = np.zeros_like(cD)
+    elif has_cA:
+        cD = np.zeros_like(cA)
 
     return _idwt(cA, cD, wavelet, mode, correct_size)
 
