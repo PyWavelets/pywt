5fe4f767293c8f93f032cf0672ddd65af55aa644
Gregory R. Lee <gregory.lee@cchmc.org>
2015-08-12 22:18:38 -0400

    TST: use precomputed results for the Matlab tests unless the full set of sizes is triggered via the environment variable PYWT_XSLOW.

--- pywt/tests/test_matlab_compatibility.py
+++ pywt/tests/test_matlab_compatibility.py
@@ -15,74 +15,122 @@ if 'PYWT_XSLOW' in os.environ:
     # Run a more comprehensive set of problem sizes.  This could take more than
     # an hour to complete.
     size_set = 'full'
+    use_precomputed = False
 else:
     size_set = 'reduced'
+    use_precomputed = True
 
-try:
-    from pymatbridge import Matlab
-    mlab = Matlab()
-    _matlab_missing = False
-except ImportError:
-    print("To run Matlab compatibility tests you need to have MathWorks "
-          "MATLAB, MathWorks Wavelet Toolbox and the pymatbridge Python "
-          "package installed.")
-    _matlab_missing = True
+if use_precomputed:
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    matlab_data_file = os.path.join(data_dir, 'dwt_matlabR2012a_result.npz')
+    matlab_result_dict = np.load(matlab_data_file)
+else:
+    try:
+        from pymatbridge import Matlab
+        mlab = Matlab()
+        _matlab_missing = False
+    except ImportError:
+        print("To run Matlab compatibility tests you need to have MathWorks "
+              "MATLAB, MathWorks Wavelet Toolbox and the pymatbridge Python "
+              "package installed.")
+        _matlab_missing = True
+
+# list of mode names in pywt and matlab
+modes = [('zpd', 'zpd'), ('cpd', 'sp0'), ('sym', 'sym'),
+         ('ppd', 'ppd'), ('sp1', 'sp1'), ('per', 'per')]
+
+families = ('db', 'sym', 'coif', 'bior', 'rbio')
+wavelets = sum([pywt.wavelist(name) for name in families], [])
 
 
-@dec.skipif(_matlab_missing)
+def _get_data_sizes(w):
+    """ Return the sizes to test for wavelet w. """
+    if size_set == 'full':
+        data_sizes = list(range(w.dec_len, 40)) + \
+            [100, 200, 500, 1000, 50000]
+    else:
+        data_sizes = (w.dec_len, w.dec_len + 1)
+    return data_sizes
+
+
+@dec.skipif(use_precomputed or _matlab_missing)
 @dec.slow
-def test_accuracy():
-    # list of mode names in pywt and matlab
-    modes = [('zpd', 'zpd'), ('cpd', 'sp0'), ('sym', 'sym'),
-             ('ppd', 'ppd'), ('sp1', 'sp1'), ('per', 'per')]
-
-    families = ('db', 'sym', 'coif', 'bior', 'rbio')
-    wavelets = sum([pywt.wavelist(name) for name in families], [])
+def test_accuracy_pymatbridge():
     rstate = np.random.RandomState(1234)
     mlab.start()
     try:
         for wavelet in wavelets:
             w = pywt.Wavelet(wavelet)
             mlab.set_variable('wavelet', wavelet)
-            if size_set == 'full':
-                data_sizes = list(range(w.dec_len, 40)) + \
-                    [100, 200, 500, 1000, 50000]
-            else:
-                data_sizes = (w.dec_len, w.dec_len + 1)
-            for N in data_sizes:
+            for N in _get_data_sizes(w):
                 data = rstate.randn(N)
                 mlab.set_variable('data', data)
                 for pmode, mmode in modes:
-                    yield _check_accuracy, data, w, pmode, mmode, wavelet
+                    ma, md = _compute_matlab_result(data, wavelet, mmode)
+                    yield _check_accuracy, data, w, pmode, ma, md, wavelet
 
     finally:
         mlab.stop()
 
 
-def _check_accuracy(data, w, pmode, mmode, wavelet):
+@dec.skipif(not use_precomputed)
+@dec.slow
+def test_accuracy_precomputed():
+    # Keep this specific random seed to match the precomputed Matlab result.
+    rstate = np.random.RandomState(1234)
+    for wavelet in wavelets:
+        w = pywt.Wavelet(wavelet)
+        for N in _get_data_sizes(w):
+            data = rstate.randn(N)
+            for pmode, mmode in modes:
+                ma, md = _load_matlab_result(data, wavelet, mmode)
+                yield _check_accuracy, data, w, pmode, ma, md, wavelet
+
+
+def _compute_matlab_result(data, wavelet, mmode):
+    """ Compute the result using MATLAB.
+
+    This function assumes that the Matlab variables `wavelet` and `data` have
+    already been set externally.
     """
-    assumes Matlab variables `wavelet` and `data` have already been set
-    externally.
+    mlab_code = "[ma, md] = dwt(data, wavelet, 'mode', '%s');" % mmode
+    res = mlab.run_code(mlab_code)
+    if not res['success']:
+        raise RuntimeError("Matlab failed to execute the provided code. "
+                           "Check that the wavelet toolbox is installed.")
+    # need np.asarray because sometimes the output is a single float64
+    ma = np.asarray(mlab.get_variable('ma'))
+    md = np.asarray(mlab.get_variable('md'))
+    return ma, md
+
+
+def _load_matlab_result(data, wavelet, mmode):
+    """ Load the precomputed result.
     """
+    N = len(data)
+    ma_key = '_'.join([mmode, wavelet, str(N), 'ma'])
+    md_key = '_'.join([mmode, wavelet, str(N), 'md'])
+    if (ma_key not in matlab_result_dict) or \
+            (md_key not in matlab_result_dict):
+        raise KeyError(
+            "Precompted Matlab result not found for wavelet: "
+            "{0}, mode: {1}, size: {2}".format(wavelet, mmode, N))
+    ma = matlab_result_dict[ma_key]
+    md = matlab_result_dict[md_key]
+    return ma, md
+
+
+def _check_accuracy(data, w, pmode, ma, md, wavelet):
+
     # max RMSE
     epsilon = 1.0e-10
 
     # PyWavelets result
     pa, pd = pywt.dwt(data, w, pmode)
 
-    # Matlab result
-    mlab_code = "[ma, md] = dwt(data, wavelet, 'mode', '%s');" % mmode
-    res = mlab.run_code(mlab_code)
-    if not res['success']:
-        raise RuntimeError("Matlab failed to execute the provided code. "
-                           "Check that the wavelet toolbox is installed.")
-    # need np.asarray because sometimes the output is type float
-    ma = np.asarray(mlab.get_variable('ma')).flat
-    md = np.asarray(mlab.get_variable('md')).flat
-
     # calculate error measures
-    rms_a = np.sqrt(np.mean((pa-ma)**2))
-    rms_d = np.sqrt(np.mean((pd-md)**2))
+    rms_a = np.sqrt(np.mean((pa - ma) ** 2))
+    rms_d = np.sqrt(np.mean((pd - md) ** 2))
 
     msg = ('[RMS_A > EPSILON] for Mode: %s, Wavelet: %s, '
            'Length: %d, rms=%.3g' % (pmode, wavelet, len(data), rms_a))
