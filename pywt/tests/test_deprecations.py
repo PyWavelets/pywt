baf7794dbc1843b5b5898dc35e914ab794734348
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-08-14 15:55:16 +0100

    TST: Modes attribute deprecation

--- pywt/tests/test_deprecations.py
+++ pywt/tests/test_deprecations.py
@@ -20,3 +20,30 @@ def test_scal2frq_deprecation():
 
 def test_orthfilt_deprecation():
     assert_warns(DeprecationWarning, pywt.orthfilt, range(6))
+
+
+old_modes = ['zpd',
+             'cpd',
+             'sym',
+             'ppd',
+             'sp1',
+             'per',
+             ]
+
+
+def test_MODES_from_object_deprecation():
+    for mode in old_modes:
+        assert_warns(DeprecationWarning, pywt.MODES.from_object, mode)
+
+
+def test_MODES_attributes_deprecation():
+    def get_mode(Modes, name):
+        return getattr(Modes, name)
+
+    for mode in old_modes:
+        assert_warns(DeprecationWarning, get_mode, pywt.Modes, mode)
+
+
+def test_MODES_attributes_usage_deprecation():
+    for mode in old_modes:
+        assert_warns(DeprecationWarning, pywt.dwt, range(10), 'db3', mode)
