b3d9d8487d36417cb691973c7ae4431fdf00e9ca
Kai Wohlfahrt <kai.scorpio@gmail.com>
2015-11-24 21:59:02 +0000

    MAINT: Split out DWT functions from _pywt.pyx
    This cut/pastes the functions relating to doing the DWT from _pywt.pyx to
    separate files. This isn't even valid python, but small changes will be done in
    a separate commit to emphasize them.

---
+++ pywt/_dwt.py
@@ -0,0 +1,215 @@
+def dwt(object data, object wavelet, object mode='symmetric'):
+    """
+    (cA, cD) = dwt(data, wavelet, mode='symmetric')
+
+    Single level Discrete Wavelet Transform.
+
+    Parameters
+    ----------
+    data : array_like
+        Input signal
+    wavelet : Wavelet object or name
+        Wavelet to use
+    mode : str, optional (default: 'symmetric')
+        Signal extension mode, see Modes
+
+    Returns
+    -------
+    (cA, cD) : tuple
+        Approximation and detail coefficients.
+
+    Notes
+    -----
+    Length of coefficients arrays depends on the selected mode:
+    for all modes except periodization:
+        len(cA) == len(cD) == floor((len(data) + wavelet.dec_len - 1) / 2)
+    for periodization mode ("per"):
+        len(cA) == len(cD) == ceil(len(data) / 2)
+
+    Examples
+    --------
+    >>> import pywt
+    >>> (cA, cD) = pywt.dwt([1, 2, 3, 4, 5, 6], 'db1')
+    >>> cA
+    [ 2.12132034  4.94974747  7.77817459]
+    >>> cD
+    [-0.70710678 -0.70710678 -0.70710678]
+
+    """
+    if np.iscomplexobj(data):
+        data = np.asarray(data)
+        cA_r, cD_r = dwt(data.real, wavelet, mode)
+        cA_i, cD_i = dwt(data.imag, wavelet, mode)
+        return  (cA_r + 1j*cA_i, cD_r + 1j*cD_i)
+
+    # accept array_like input; make a copy to ensure a contiguous array
+    dt = _check_dtype(data)
+    data = np.array(data, dtype=dt)
+    if data.ndim != 1:
+        raise ValueError("dwt requires a 1D data array.")
+    return _dwt(data, wavelet, mode)
+
+
+def downcoef(part, data, wavelet, mode='symmetric', level=1):
+    """
+    downcoef(part, data, wavelet, mode='symmetric', level=1)
+
+    Partial Discrete Wavelet Transform data decomposition.
+
+    Similar to `pywt.dwt`, but computes only one set of coefficients.
+    Useful when you need only approximation or only details at the given level.
+
+    Parameters
+    ----------
+    part : str
+        Coefficients type:
+
+        * 'a' - approximations reconstruction is performed
+        * 'd' - details reconstruction is performed
+
+    data : array_like
+        Input signal.
+    wavelet : Wavelet object or name
+        Wavelet to use
+    mode : str, optional
+        Signal extension mode, see `Modes`.  Default is 'symmetric'.
+    level : int, optional
+        Decomposition level.  Default is 1.
+
+    Returns
+    -------
+    coeffs : ndarray
+        1-D array of coefficients.
+
+    See Also
+    --------
+    upcoef
+
+    """
+    if np.iscomplexobj(data):
+        return (downcoef(part, data.real, wavelet, mode, level) +
+                1j*downcoef(part, data.imag, wavelet, mode, level))
+    # accept array_like input; make a copy to ensure a contiguous array
+    dt = _check_dtype(data)
+    data = np.array(data, dtype=dt)
+    return _downcoef(part, data, wavelet, mode, level)
+
+
+def idwt(cA, cD, object wavelet, object mode='symmetric'):
+    """
+    idwt(cA, cD, wavelet, mode='symmetric')
+
+    Single level Inverse Discrete Wavelet Transform
+
+    Parameters
+    ----------
+    cA : array_like or None
+        Approximation coefficients.  If None, will be set to array of zeros
+        with same shape as `cD`.
+    cD : array_like or None
+        Detail coefficients.  If None, will be set to array of zeros
+        with same shape as `cA`.
+    wavelet : Wavelet object or name
+        Wavelet to use
+    mode : str, optional (default: 'symmetric')
+        Signal extension mode, see Modes
+
+    Returns
+    -------
+    rec: array_like
+        Single level reconstruction of signal from given coefficients.
+
+    """
+    # accept array_like input; make a copy to ensure a contiguous array
+
+    if cA is None and cD is None:
+        raise ValueError("At least one coefficient parameter must be "
+                         "specified.")
+
+    # for complex inputs: compute real and imaginary separately then combine
+    if ((cA is not None) and np.iscomplexobj(cA)) or ((cD is not None) and
+            np.iscomplexobj(cD)):
+        if cA is None:
+            cD = np.asarray(cD)
+            cA = np.zeros_like(cD)
+        elif cD is None:
+            cA = np.asarray(cA)
+            cD = np.zeros_like(cA)
+        return (idwt(cA.real, cD.real, wavelet, mode) +
+                1j*idwt(cA.imag, cD.imag, wavelet, mode))
+
+    if cA is not None:
+        dt = _check_dtype(cA)
+        cA = np.array(cA, dtype=dt)
+        if cA.ndim != 1:
+            raise ValueError("idwt requires 1D coefficient arrays.")
+    if cD is not None:
+        dt = _check_dtype(cD)
+        cD = np.array(cD, dtype=dt)
+        if cD.ndim != 1:
+            raise ValueError("idwt requires 1D coefficient arrays.")
+
+    if cA is not None and cD is not None:
+        if cA.dtype != cD.dtype:
+            # need to upcast to common type
+            cA = cA.astype(np.float64)
+            cD = cD.astype(np.float64)
+    elif cA is None:
+        cA = np.zeros_like(cD)
+    elif cD is None:
+        cD = np.zeros_like(cA)
+
+    return _idwt(cA, cD, wavelet, mode)
+
+
+def upcoef(part, coeffs, wavelet, level=1, take=0):
+    """
+    upcoef(part, coeffs, wavelet, level=1, take=0)
+
+    Direct reconstruction from coefficients.
+
+    Parameters
+    ----------
+    part : str
+        Coefficients type:
+        * 'a' - approximations reconstruction is performed
+        * 'd' - details reconstruction is performed
+    coeffs : array_like
+        Coefficients array to recontruct
+    wavelet : Wavelet object or name
+        Wavelet to use
+    level : int, optional
+        Multilevel reconstruction level.  Default is 1.
+    take : int, optional
+        Take central part of length equal to 'take' from the result.
+        Default is 0.
+
+    Returns
+    -------
+    rec : ndarray
+        1-D array with reconstructed data from coefficients.
+
+    See Also
+    --------
+    downcoef
+
+    Examples
+    --------
+    >>> import pywt
+    >>> data = [1,2,3,4,5,6]
+    >>> (cA, cD) = pywt.dwt(data, 'db2', 'smooth')
+    >>> pywt.upcoef('a', cA, 'db2') + pywt.upcoef('d', cD, 'db2')
+    [-0.25       -0.4330127   1.          2.          3.          4.          5.
+      6.          1.78589838 -1.03108891]
+    >>> n = len(data)
+    >>> pywt.upcoef('a', cA, 'db2', take=n) + pywt.upcoef('d', cD, 'db2', take=n)
+    [ 1.  2.  3.  4.  5.  6.]
+
+    """
+    if np.iscomplexobj(coeffs):
+        return (upcoef(part, coeffs.real, wavelet, level, take) +
+                1j*upcoef(part, coeffs.imag, wavelet, level, take))
+    # accept array_like input; make a copy to ensure a contiguous array
+    dt = _check_dtype(coeffs)
+    coeffs = np.array(coeffs, dtype=dt)
+    return _upcoef(part, coeffs, wavelet, level, take)
---
+++ pywt/src/_dwt.pyx
@@ -0,0 +1,328 @@
+def _dwt(np.ndarray[data_t, ndim=1] data, object wavelet, object mode='symmetric'):
+    """See `dwt` docstring for details."""
+    cdef np.ndarray[data_t, ndim=1, mode="c"] cA, cD
+    cdef Wavelet w
+    cdef common.MODE mode_
+
+    w = c_wavelet_from_object(wavelet)
+    mode_ = _try_mode(mode)
+
+    data = np.array(data)
+    output_len = common.dwt_buffer_length(data.size, w.dec_len, mode_)
+    if output_len < 1:
+        raise RuntimeError("Invalid output length.")
+
+    cA = np.zeros(output_len, data.dtype)
+    cD = np.zeros(output_len, data.dtype)
+
+    if data_t == np.float64_t:
+        if (c_wt.double_dec_a(&data[0], data.size, w.w,
+                              &cA[0], cA.size, mode_) < 0
+            or
+            c_wt.double_dec_d(&data[0], data.size, w.w,
+                              &cD[0], cD.size, mode_) < 0):
+            raise RuntimeError("C dwt failed.")
+    elif data_t == np.float32_t:
+        if (c_wt.float_dec_a(&data[0], data.size, w.w,
+                             &cA[0], cA.size, mode_) < 0
+            or
+            c_wt.float_dec_d(&data[0], data.size, w.w,
+                             &cD[0], cD.size, mode_) < 0):
+            raise RuntimeError("C dwt failed.")
+    else:
+        raise RuntimeError("Invalid data type.")
+
+    return (cA, cD)
+
+
+def _downcoef(part, np.ndarray[data_t, ndim=1, mode="c"] data,
+              object wavelet, object mode='symmetric', int level=1):
+    cdef np.ndarray[data_t, ndim=1, mode="c"] coeffs
+    cdef int i, do_dec_a
+    cdef index_t dec_len
+    cdef Wavelet w
+    cdef common.MODE mode_
+
+    w = c_wavelet_from_object(wavelet)
+    mode_ = _try_mode(mode)
+
+    if part not in ('a', 'd'):
+        raise ValueError("Argument 1 must be 'a' or 'd', not '%s'." % part)
+    do_dec_a = (part == 'a')
+
+    if level < 1:
+        raise ValueError("Value of level must be greater than 0.")
+
+    for i in range(level):
+        output_len = common.dwt_buffer_length(data.size, w.dec_len, mode_)
+        if output_len < 1:
+            raise RuntimeError("Invalid output length.")
+        coeffs = np.zeros(output_len, dtype=data.dtype)
+
+        # To mirror multi-level wavelet decomposition behaviour, when detail
+        # coefficients are requested, the dec_d variant is only called at the
+        # final level.  All prior levels use dec_a.  In other words, the detail
+        # coefficients at level n are those produced via the operation of the
+        # detail filter on the approximation coefficients of level n-1.
+        if do_dec_a or (i < level - 1):
+            if data_t is np.float64_t:
+                if c_wt.double_dec_a(&data[0], data.size, w.w,
+                                     &coeffs[0], coeffs.size, mode_) < 0:
+                    raise RuntimeError("C dec_a failed.")
+            elif data_t is np.float32_t:
+                if c_wt.float_dec_a(&data[0], data.size, w.w,
+                                    &coeffs[0], coeffs.size, mode_) < 0:
+                    raise RuntimeError("C dec_a failed.")
+            else:
+                raise RuntimeError("Invalid data type.")
+        else:
+            if data_t is np.float64_t:
+                if c_wt.double_dec_d(&data[0], data.size, w.w,
+                                     &coeffs[0], coeffs.size, mode_) < 0:
+                    raise RuntimeError("C dec_d failed.")
+            elif data_t is np.float32_t:
+                if c_wt.float_dec_d(&data[0], data.size, w.w,
+                                    &coeffs[0], coeffs.size, mode_) < 0:
+                    raise RuntimeError("C dec_d failed.")
+            else:
+                raise RuntimeError("Invalid data type.")
+        data = coeffs
+
+    return coeffs
+
+
+cpdef dwt_axis(np.ndarray data, object wavelet, object mode='symmetric', unsigned int axis=0):
+    cdef Wavelet w = c_wavelet_from_object(wavelet)
+    cdef common.MODE _mode = _try_mode(mode)
+    cdef common.ArrayInfo data_info, output_info
+    cdef np.ndarray cD, cA
+    cdef size_t[::1] output_shape
+
+    data = data.astype(_check_dtype(data), copy=False)
+
+    output_shape = (<size_t [:data.ndim]> <size_t *> data.shape).copy()
+    output_shape[axis] = common.dwt_buffer_length(data.shape[axis], w.dec_len, _mode)
+
+    cA = np.empty(output_shape, data.dtype)
+    cD = np.empty(output_shape, data.dtype)
+
+    data_info.ndim = data.ndim
+    data_info.strides = <index_t *> data.strides
+    data_info.shape = <size_t *> data.shape
+
+    output_info.ndim = cA.ndim
+    output_info.strides = <index_t *> cA.strides
+    output_info.shape = <size_t *> cA.shape
+
+    if data.dtype == np.float64:
+        if c_wt.double_downcoef_axis(<double *> data.data, data_info,
+                                     <double *> cA.data, output_info,
+                                     w.w, axis, common.COEF_APPROX, _mode):
+            raise RuntimeError("C wavelet transform failed")
+        if c_wt.double_downcoef_axis(<double *> data.data, data_info,
+                                     <double *> cD.data, output_info,
+                                     w.w, axis, common.COEF_DETAIL, _mode):
+            raise RuntimeError("C wavelet transform failed")
+    elif data.dtype == np.float32:
+        if c_wt.float_downcoef_axis(<float *> data.data, data_info,
+                                    <float *> cA.data, output_info,
+                                    w.w, axis, common.COEF_APPROX, _mode):
+            raise RuntimeError("C wavelet transform failed")
+        if c_wt.float_downcoef_axis(<float *> data.data, data_info,
+                                    <float *> cD.data, output_info,
+                                    w.w, axis, common.COEF_DETAIL, _mode):
+            raise RuntimeError("C wavelet transform failed")
+    else:
+        raise TypeError("Array must be floating point, not {}"
+                        .format(data.dtype))
+    return (cA, cD)
+
+
+def _idwt(np.ndarray[data_t, ndim=1, mode="c"] cA,
+          np.ndarray[data_t, ndim=1, mode="c"] cD,
+          object wavelet, object mode='symmetric'):
+    """See `idwt` for details"""
+
+    cdef index_t input_len
+
+    cdef Wavelet w
+    cdef common.MODE mode_
+
+    w = c_wavelet_from_object(wavelet)
+    mode_ = _try_mode(mode)
+
+    cdef np.ndarray[data_t, ndim=1, mode="c"] rec
+    cdef index_t rec_len
+
+    # check for size difference between arrays
+    if cA.size != cD.size:
+        raise ValueError("Coefficients arrays must have the same size.")
+    else:
+        input_len = cA.size
+
+    # find reconstruction buffer length
+    rec_len = common.idwt_buffer_length(input_len, w.rec_len, mode_)
+    if rec_len < 1:
+        msg = ("Invalid coefficient arrays length for specified wavelet. "
+               "Wavelet and mode must be the same as used for decomposition.")
+        raise ValueError(msg)
+
+    # allocate buffer
+    if cA is not None:
+        rec = np.zeros(rec_len, dtype=cA.dtype)
+    else:
+        rec = np.zeros(rec_len, dtype=cD.dtype)
+
+    # call idwt func.  one of cA/cD can be None, then only
+    # reconstruction of non-null part will be performed
+    if data_t is np.float64_t:
+        if c_wt.double_idwt(&cA[0], cA.size,
+                            &cD[0], cD.size,
+                            &rec[0], rec.size,
+                            w.w, mode_) < 0:
+            raise RuntimeError("C idwt failed.")
+    elif data_t == np.float32_t:
+        if c_wt.float_idwt(&cA[0], cA.size,
+                           &cD[0], cD.size,
+                           &rec[0], rec.size,
+                           w.w, mode_) < 0:
+            raise RuntimeError("C idwt failed.")
+    else:
+        raise RuntimeError("Invalid data type.")
+
+    return rec
+
+
+def _upcoef(part, np.ndarray[data_t, ndim=1, mode="c"] coeffs, wavelet,
+            int level=1, int take=0):
+    cdef Wavelet w
+    cdef np.ndarray[data_t, ndim=1, mode="c"] rec
+    cdef int i, do_rec_a
+    cdef index_t rec_len, left_bound, right_bound
+
+    rec_len = 0
+
+    if part not in ('a', 'd'):
+        raise ValueError("Argument 1 must be 'a' or 'd', not '%s'." % part)
+    do_rec_a = (part == 'a')
+
+    w = c_wavelet_from_object(wavelet)
+
+    if level < 1:
+        raise ValueError("Value of level must be greater than 0.")
+
+    for i in range(level):
+        # output len
+        rec_len = common.reconstruction_buffer_length(coeffs.size, w.dec_len)
+        if rec_len < 1:
+            raise RuntimeError("Invalid output length.")
+
+        # reconstruct
+        rec = np.zeros(rec_len, dtype=coeffs.dtype)
+
+        # To mirror multi-level wavelet reconstruction behaviour, when detail
+        # reconstruction is requested, the dec_d variant is only called at the
+        # first level to generate the approximation coefficients at the second
+        # level.  Subsequent levels apply the reconstruction filter.
+        if do_rec_a:
+            if data_t is np.float64_t:
+                if c_wt.double_rec_a(&coeffs[0], coeffs.size, w.w,
+                                     &rec[0], rec.size) < 0:
+                    raise RuntimeError("C rec_a failed.")
+            elif data_t is np.float32_t:
+                if c_wt.float_rec_a(&coeffs[0], coeffs.size, w.w,
+                                    &rec[0], rec.size) < 0:
+                    raise RuntimeError("C rec_a failed.")
+            else:
+                raise RuntimeError("Invalid data type.")
+        else:
+            if data_t is np.float64_t:
+                if c_wt.double_rec_d(&coeffs[0], coeffs.size, w.w,
+                                     &rec[0], rec.size) < 0:
+                    raise RuntimeError("C rec_d failed.")
+            elif data_t is np.float32_t:
+                if c_wt.float_rec_d(&coeffs[0], coeffs.size, w.w,
+                                    &rec[0], rec.size) < 0:
+                    raise RuntimeError("C rec_d failed.")
+            else:
+                raise RuntimeError("Invalid data type.")
+            # switch to approximation filter for subsequent levels
+            do_rec_a = 1
+
+        # TODO: this algorithm needs some explaining
+        coeffs = rec
+
+    if take > 0 and take < rec_len:
+        left_bound = right_bound = (rec_len-take) // 2
+        if (rec_len-take) % 2:
+            # right_bound must never be zero for indexing to work
+            right_bound = right_bound + 1
+
+        return rec[left_bound:-right_bound]
+
+    return rec
+
+
+cpdef idwt_axis(np.ndarray coefs_a, np.ndarray coefs_d, object wavelet,
+                object mode='symmetric', unsigned int axis=0):
+    cdef Wavelet w = c_wavelet_from_object(wavelet)
+    cdef common.ArrayInfo a_info, d_info, output_info
+    cdef np.ndarray output
+    cdef np.dtype output_dtype
+    cdef size_t[::1] output_shape
+    cdef common.MODE _mode = _try_mode(mode)
+
+    if coefs_a is not None:
+        if coefs_d is not None and coefs_d.dtype.itemsize > coefs_a.dtype.itemsize:
+            coefs_a = coefs_a.astype(_check_dtype(coefs_d), copy=False)
+        else:
+            coefs_a = coefs_a.astype(_check_dtype(coefs_a), copy=False)
+        a_info.ndim = coefs_a.ndim
+        a_info.strides = <index_t *> coefs_a.strides
+        a_info.shape = <size_t *> coefs_a.shape
+    if coefs_d is not None:
+        if coefs_a is not None and coefs_a.dtype.itemsize > coefs_d.dtype.itemsize:
+            coefs_d = coefs_d.astype(_check_dtype(coefs_a), copy=False)
+        else:
+            coefs_d = coefs_d.astype(_check_dtype(coefs_d), copy=False)
+        d_info.ndim = coefs_d.ndim
+        d_info.strides = <index_t *> coefs_d.strides
+        d_info.shape = <size_t *> coefs_d.shape
+
+    if coefs_a is not None:
+        output_shape = (<size_t [:coefs_a.ndim]> <size_t *> coefs_a.shape).copy()
+        output_shape[axis] = common.idwt_buffer_length(coefs_a.shape[axis],
+                                                       w.rec_len, _mode)
+        output_dtype = coefs_a.dtype
+    elif coefs_d is not None:
+        output_shape = (<size_t [:coefs_d.ndim]> <size_t *> coefs_d.shape).copy()
+        output_shape[axis] = common.idwt_buffer_length(coefs_d.shape[axis],
+                                                       w.rec_len, _mode)
+        output_dtype = coefs_d.dtype
+    else:
+        return None;
+
+    output = np.empty(output_shape, output_dtype)
+
+    output_info.ndim = output.ndim
+    output_info.strides = <index_t *> output.strides
+    output_info.shape = <size_t *> output.shape
+
+    if output.dtype == np.float64:
+        if c_wt.double_idwt_axis(<double *> coefs_a.data if coefs_a is not None else NULL,
+                                 &a_info if coefs_a is not None else NULL,
+                                 <double *> coefs_d.data if coefs_d is not None else NULL,
+                                 &d_info if coefs_d is not None else NULL,
+                                 <double *> output.data, output_info,
+                                 w.w, axis, _mode):
+            raise RuntimeError("C inverse wavelet transform failed")
+    if output.dtype == np.float32:
+        if c_wt.float_idwt_axis(<float *> coefs_a.data if coefs_a is not None else NULL,
+                                &a_info if coefs_a is not None else NULL,
+                                <float *> coefs_d.data if coefs_d is not None else NULL,
+                                &d_info if coefs_d is not None else NULL,
+                                <float *> output.data, output_info,
+                                w.w, axis, _mode):
+            raise RuntimeError("C inverse wavelet transform failed")
+
+    return output
