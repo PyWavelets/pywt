7e2e605c8b44b5639fb04d5090749f6f751aa923
Kai Wohlfahrt <kai.scorpio@gmail.com>
2015-11-25 11:34:30 +0000

    MAINT: Typo in test name

--- pywt/tests/test_multidim.py
+++ pywt/tests/test_multidim.py
@@ -238,7 +238,7 @@ def test_idwtn_axis():
     assert_allclose(pywt.idwtn(coefs, 'haar', axis=(1, 1)), data, atol=1e-14)
 
 
-def test_netagive_axis():
+def test_negative_axis():
     data = np.array([[0, 1, 2, 3],
                      [1, 1, 1, 1],
                      [1, 4, 2, 8]])
