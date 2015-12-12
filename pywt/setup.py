9e352a00e74fe582a911665f20edf51189fac456
Kai Wohlfahrt <kai.scorpio@gmail.com>
2015-12-12 17:14:50 +0000

    BLD: Add setup.py to _extensions

--- pywt/setup.py
+++ pywt/setup.py
@@ -9,47 +9,7 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('pywt', parent_package, top_path)
 
     config.add_data_dir('tests')
-
-    sources = ["c/common", "c/convolution", "c/wavelets", "c/wt"]
-    source_templates = ["c/convolution", "c/wt"]
-    headers = ["c/templating", "c/wavelets_coeffs"]
-    header_templates = ["c/convolution", "c/wt", "c/wavelets_coeffs"]
-
-    c_files = ["_extensions/{0}.c".format(s) for s in sources]
-    depends = (["_extensions/{0}.template.c".format(s) for s in source_templates]
-               + ["_extensions/{0}.template.h".format(s) for s in header_templates]
-               + ["_extensions/{0}.h".format(s) for s in headers]
-               + ["_extensions/{0}.h".format(s) for s in sources])
-
-    config.add_subpackage(
-        '_extensions',
-        subpackage_path="_extensions",
-    )
-
-    config.add_extension(
-        '_extensions._pywt',
-        sources=["_extensions/_pywt.c"] + c_files,
-        depends=["_extensions/_dwt.c"] + depends,
-        include_dirs=["_extensions", "_extensions/c", np.get_include()],
-        define_macros=[("PY_EXTENSION", None)],
-    )
-
-    config.add_extension(
-        '_extensions._dwt',
-        sources=["_extensions/_dwt.c"] + c_files,
-        depends=["_extensions/_pywt.c"] + depends,
-        include_dirs=["_extensions", "_extensions/c", np.get_include()],
-        define_macros=[("PY_EXTENSION", None)],
-    )
-
-    config.add_extension(
-        '_extensions._swt',
-        sources=["_extensions/_swt.c"] + c_files,
-        depends=["_extensions/_pywt.c"] + depends,
-        include_dirs=["_extensions", "_extensions/c", np.get_include()],
-        define_macros=[("PY_EXTENSION", None)],
-    )
-
+    config.add_subpackage('_extensions')
     config.make_config_py()
     return config
 
