b4d3678e38ef0452d47c62d5bf62791396160538
Aaron O'Leary <aaron.oleary@gmail.com>
2015-12-14 14:34:36 +0000

    MAINT: (re)add single axis dwt

--- pywt/src/_pywt.pyx
+++ pywt/src/_pywt.pyx
@@ -755,6 +755,43 @@ def _dwt(np.ndarray data, object wavelet, object mode='symmetric', int axis=-1):
     return (cA, cD)
 
 
+def dwt_single(np.ndarray[data_t, ndim=1] data, object wavelet,
+                                                object mode='symmetric'):
+    cdef np.ndarray[data_t, ndim=1, mode="c"] cA, cD
+    cdef Wavelet w = c_wavelet_from_object(wavelet)
+    cdef common.MODE mode_ = _try_mode(mode)
+
+    cdef size_t output_len
+
+    data = data.astype(_check_dtype(data), copy=True)
+
+    output_len = common.dwt_buffer_length(data.size, w.dec_len, mode_)
+    if output_len < 1:
+        raise RuntimeError("Invalid output length.")
+
+    cA = np.zeros(output_len, data.dtype)
+    cD = np.zeros(output_len, data.dtype)
+
+    if data_t == np.float64_t:
+        if (c_wt.double_dec_a(&data[0], data.size, w.w,
+                              &cA[0], cA.size, mode_) < 0
+            or
+            c_wt.double_dec_d(&data[0], data.size, w.w,
+                              &cD[0], cD.size, mode_) < 0):
+            raise RuntimeError("C dwt failed.")
+    elif data_t == np.float32_t:
+        if (c_wt.float_dec_a(&data[0], data.size, w.w,
+                             &cA[0], cA.size, mode_) < 0
+            or
+            c_wt.float_dec_d(&data[0], data.size, w.w,
+                             &cD[0], cD.size, mode_) < 0):
+            raise RuntimeError("C dwt failed.")
+    else:
+        raise RuntimeError("Invalid data type.")
+
+    return (cA, cD)
+
+
 cpdef dwt_axis(np.ndarray data, object wavelet, object mode='symmetric', unsigned int axis=0):
     cdef Wavelet w = c_wavelet_from_object(wavelet)
     cdef common.MODE _mode = _try_mode(mode)
