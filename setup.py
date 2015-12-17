e4bf3ee44b752876b95ec5bf361ff5c39ca3e3f4
Aaron O'Leary <aaron.oleary@gmail.com>
2015-12-17 00:56:34 +0000

    MAINT: include data submodule in install

--- setup.py
+++ setup.py
@@ -131,6 +131,8 @@ def configuration(parent_package='',top_path=None):
                        quiet=True)
 
     config.add_subpackage('pywt')
+    config.add_subpackage('pywt/data')
+    config.add_data_files('pywt/data/*dat')
 
     config.get_version('pywt/version.py')
     return config
