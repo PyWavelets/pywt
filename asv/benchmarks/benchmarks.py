ea096a0aefb67398f2842c686f3b1a6a6c736902
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-02-28 23:04:50 +0000

    ENH: Add initial benchmarks for dwt
    Interestingly, 1D dwt is significantly slower on python 3.

--- asv/benchmarks/benchmarks.py
+++ asv/benchmarks/benchmarks.py
@@ -1,36 +1,59 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import numpy as np
+import pywt
 
-class TimeSuite:
+class DwtTimeSuiteBase(object):
     """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
+    Set-up for (I)DWT timing.
     """
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+    params = ([16, 100, 101, 256, 490, 491, 512],
+              ['haar', 'db4', 'sym20'],)
+    param_names = ('n', 'wavelet')
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+    def setup(self, n, wavelet):
+        self.data = np.ones(n, dtype='float')
 
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+class DwtTimeSuite(DwtTimeSuiteBase):
+    def time_dwt(self, n, wavelet):
+        pywt.dwt(self.data, wavelet)
+
+
+class IdwtTimeSuite(DwtTimeSuiteBase):
+    def setup(self, n, wavelet):
+        super(IdwtTimeSuite, self).setup(n, wavelet)
+        self.cA, self.cD = pywt.dwt(self.data, wavelet)
+
+
+    def time_idwt(self, n, wavelet):
+        pywt.idwt(self.cA, self.cD, wavelet)
+
+
+class DwtnTimeSuiteBase(object):
+    """
+    Set-up for (I)DWTN timing.
+    """
+    params = ([1, 2, 3],
+              [16, 100, 101, 256],
+              ['haar', 'db4', 'sym20'],)
+    param_names = ('D', 'n', 'wavelet')
+
+    def setup(self, D, n, wavelet):
+        self.data = np.ones((n,) * D, dtype='float')
+
+
+class DwtnTimeSuite(DwtnTimeSuiteBase):
+    def time_dwtn(self, D, n, wavelet):
+        pywt.dwtn(self.data, wavelet)
+
+
+class IdwtnTimeSuite(DwtnTimeSuiteBase):
+    def setup(self, D, n, wavelet):
+        super(IdwtnTimeSuite, self).setup(D, n, wavelet)
+        self.data = pywt.dwtn(self.data, wavelet)
+
+
+    def time_idwtn(self, D, n, wavelet):
+        pywt.idwtn(self.data, wavelet)
