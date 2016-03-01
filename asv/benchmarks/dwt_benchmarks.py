41b8ffa120d04b460a2d09402e85cb7b6e6437a1
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-03-01 17:47:00 +0000

    ENH: Benchmark _extensions._dwt, with modes
    Benchmarking these functions should hopefully give a closer view of how
    performance in the C core changes without requiring a lot of effort to
    instrument them separately.
    
    The different modes should have somewhat different performance, especially
    for small sizes/large filters. If this doesn't happen, there is too much
    overhead to benchmark the convolution directly and this needs to be done
    separately.
    
    modes are only set for 1D, since n-dimensional path is not affected by the
    mode (it is just passed through to the 1D).

--- asv/benchmarks/dwt_benchmarks.py
+++ asv/benchmarks/dwt_benchmarks.py
@@ -7,26 +7,34 @@ class DwtTimeSuiteBase(object):
     Set-up for (I)DWT timing.
     """
     params = ([16, 100, 101, 256, 490, 491, 512],
-              ['haar', 'db4', 'sym20'],)
-    param_names = ('n', 'wavelet')
+              ['haar', 'db4', 'sym20'],
+              pywt.Modes.modes)
+    param_names = ('n', 'wavelet', 'modes')
 
-    def setup(self, n, wavelet):
+    def setup(self, n, wavelet, mode):
         self.data = np.ones(n, dtype='float')
+        self.wavelet = pywt.Wavelet(wavelet)
+        self.mode = pywt.Modes.from_object(mode)
 
 
 class DwtTimeSuite(DwtTimeSuiteBase):
-    def time_dwt(self, n, wavelet):
-        pywt.dwt(self.data, wavelet)
+    def time_dwt(self, n, wavelet, mode):
+        pywt.dwt(self.data, wavelet, mode)
+
+    def time_dwt_ext(self, n, wavelet, mode):
+        pywt._extensions._dwt.dwt_single(self.data, self.wavelet, self.mode)
 
 
 class IdwtTimeSuite(DwtTimeSuiteBase):
-    def setup(self, n, wavelet):
-        super(IdwtTimeSuite, self).setup(n, wavelet)
-        self.cA, self.cD = pywt.dwt(self.data, wavelet)
+    def setup(self, n, wavelet, mode):
+        super(IdwtTimeSuite, self).setup(n, wavelet, mode)
+        self.cA, self.cD = pywt.dwt(self.data, wavelet, mode)
 
+    def time_idwt(self, n, wavelet, mode):
+        pywt.idwt(self.cA, self.cD, wavelet, mode)
 
-    def time_idwt(self, n, wavelet):
-        pywt.idwt(self.cA, self.cD, wavelet)
+    def time_idwt_ext(self, n, wavelet, mode):
+        pywt._extensions._dwt.idwt_single(self.cA, self.cD, self.wavelet, self.mode)
 
 
 class DwtnTimeSuiteBase(object):
@@ -51,7 +59,6 @@ class IdwtnTimeSuite(DwtnTimeSuiteBase):
     def setup(self, D, n, wavelet):
         super(IdwtnTimeSuite, self).setup(D, n, wavelet)
         self.data = pywt.dwtn(self.data, wavelet)
-
 
     def time_idwtn(self, D, n, wavelet):
         pywt.idwtn(self.data, wavelet)
