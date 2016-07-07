8f018cc477ffa9d183dbcd8942ebde1f319ee8b3
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2016-07-07 20:08:49 +0200

    removed scipy from example

--- pywt/_cwt.py
+++ pywt/_cwt.py
@@ -49,10 +49,9 @@ def cwt(data, scales, wavelet, sampling_period=1.):
     ----------
     >>> import pywt
     >>> import numpy as np
-    >>> from scipy import signal
     >>> import matplotlib.pyplot as plt
     >>> t = np.linspace(-1, 1, 200, endpoint=False)
-    >>> sig  = np.cos(2 * np.pi * 7 * t) + signal.gausspulse(t - 0.4, fc=2)
+    >>> sig  = np.cos(2 * np.pi * 7 * t) + np.real(np.exp(-7*(t-0.4)**2)*np.exp(1j*2*np.pi*2*(t-0.4)))
     >>> widths = np.arange(1, 31)
     >>> cwtmatr, freqs = pywt.cwt(sig, widths, 'mexh')
     >>> plt.imshow(cwtmatr, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
