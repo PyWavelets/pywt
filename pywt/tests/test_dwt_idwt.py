57b21c42e21c6f4afb248c0b3d0cfa9c06550872
Gregory R. Lee <grlee77@gmail.com>
2016-09-09 03:30:24 -0400

    TST: update test_dwt_coeff_len to include the new edge mode

--- pywt/tests/test_dwt_idwt.py
+++ pywt/tests/test_dwt_idwt.py
@@ -91,10 +91,10 @@ def test_dwt_coeff_len():
     w = pywt.Wavelet('sym3')
     ln_modes = [pywt.dwt_coeff_len(len(x), w.dec_len, mode) for mode in
                 pywt.Modes.modes]
-    assert_allclose(ln_modes, [6, 6, 6, 6, 6, 4])
+    assert_allclose(ln_modes, [6, 6, 6, 6, 6, 6, 4])
     ln_modes = [pywt.dwt_coeff_len(len(x), w, mode) for mode in
                 pywt.Modes.modes]
-    assert_allclose(ln_modes, [6, 6, 6, 6, 6, 4])
+    assert_allclose(ln_modes, [6, 6, 6, 6, 6, 6, 4])
 
 
 def test_idwt_none_input():
