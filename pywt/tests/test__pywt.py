18fc2bbf16b4d89eecee230d007aca3739c685e7
Gregory R. Lee <grlee77@gmail.com>
2017-01-09 15:19:46 -0500

    ENH: dwt_max_level: also allow the name of a wavelet to be used as the 2nd argument.
    update docstring to indicate that a wavelet can be passed in place of the filter length
    
    raise ValueError if filter_len < 2
    
    formerly filter_len = 0 or 1 returned 0 while a negative filter_len raised an OverflowError
    
    raise informative ValueError on unrecognized string or non-integer input

--- pywt/tests/test__pywt.py
+++ pywt/tests/test__pywt.py
@@ -106,7 +106,21 @@ def test_dwt_max_level():
     assert_(pywt.dwt_max_level(16, 8) == 1)
     assert_(pywt.dwt_max_level(16, 9) == 1)
     assert_(pywt.dwt_max_level(16, 10) == 0)
+    assert_(pywt.dwt_max_level(16, np.int8(10)) == 0)
+    assert_(pywt.dwt_max_level(16, 10.) == 0)
     assert_(pywt.dwt_max_level(16, 18) == 0)
+
+    # accepts discrete Wavelet object or string as well
+    assert_(pywt.dwt_max_level(32, pywt.Wavelet('sym5')) == 1)
+    assert_(pywt.dwt_max_level(32, 'sym5') == 1)
+
+    # string input that is not a discrete wavelet
+    assert_raises(ValueError, pywt.dwt_max_level, 16, 'mexh')
+
+    # filter_len must be an integer >= 2
+    assert_raises(ValueError, pywt.dwt_max_level, 16, 1)
+    assert_raises(ValueError, pywt.dwt_max_level, 16, -1)
+    assert_raises(ValueError, pywt.dwt_max_level, 16, 3.3)
 
 
 def test_ContinuousWavelet_errs():
