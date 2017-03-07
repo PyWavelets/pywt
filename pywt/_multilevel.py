7502b531476acb2dc9103fae21108051b4f35f12
Gregory R. Lee <grlee77@gmail.com>
2017-03-07 17:25:15 -0500

    guard against invalid axis index in waverecn
    The ValueError raised is the same as the one in idwt

--- pywt/_multilevel.py
+++ pywt/_multilevel.py
@@ -143,10 +143,13 @@ def waverec(coeffs, wavelet, mode='symmetric', axis=-1):
 
     for d in ds:
         if (a is not None) and (d is not None):
-            if a.shape[axis] == d.shape[axis] + 1:
-                a = a[[slice(s) for s in d.shape]]
-            elif a.shape[axis] != d.shape[axis]:
-                raise RuntimeError("coefficient shape mismatch")
+            try:
+                if a.shape[axis] == d.shape[axis] + 1:
+                    a = a[[slice(s) for s in d.shape]]
+                elif a.shape[axis] != d.shape[axis]:
+                    raise ValueError("coefficient shape mismatch")
+            except IndexError:
+                raise ValueError("Axis greater than coefficient dimensions")
         a = idwt(a, d, wavelet, mode, axis)
 
     return a
