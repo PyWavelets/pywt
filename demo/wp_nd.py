446ffce155a6e17b973f7d59d2842b6d1854ed1a
Gregory R. Lee <gregory.lee@cchmc.org>
2018-03-01 20:43:54 -0500

    misc cleanup

--- demo/wp_nd.py
+++ demo/wp_nd.py
@@ -16,26 +16,6 @@ wp2 = WaveletPacketND(arr, 'db2', 'symmetric', maxlevel=maxlevel)
 # Show original figure
 plt.imshow(arr, interpolation="nearest", cmap=plt.cm.gray)
 
-# path = ['aa', 'ad', 'da', 'dd']
-
-# # Show level 1 nodes
-# fig = plt.figure()
-# for i, p2 in enumerate(path):
-#     ax = fig.add_subplot(2, 2, i + 1)
-#     ax.imshow(np.sqrt(np.abs(wp2[p2].data)), origin='image',
-#               interpolation="nearest", cmap=plt.cm.gray)
-#     ax.set_title(p2)
-
-# # Show level 2 nodes
-# for p1 in path:
-#     fig = plt.figure()
-#     for i, p2 in enumerate(path):
-#         ax = fig.add_subplot(2, 2, i + 1)
-#         p1p2 = p1 + p2
-#         ax.imshow(np.sqrt(np.abs(wp2[p1p2].data)), origin='image',
-#                   interpolation="nearest", cmap=plt.cm.gray)
-#         ax.set_title(p1p2)
-
 fig = plt.figure()
 i = 1
 nsubplots = len(wp2.get_level(maxlevel, 'natural'))
