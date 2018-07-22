a23a6d4edc01b968059019c97ad20a401dca7103
Ralf Gommers <ralf.gommers@gmail.com>
2018-07-21 17:50:10 -0700

    MAINT: update Python version support.  Closes gh-385.

--- setup.py
+++ setup.py
@@ -280,9 +280,9 @@ if __name__ == '__main__':
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Topic :: Software Development :: Libraries :: Python Modules"
         ],
         platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
