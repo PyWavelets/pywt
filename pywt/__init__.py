05fbd25f3e14f824519f87131b4c153fb2737e21
Gregory Lee <grlee77@gmail.com>
2019-03-27 19:28:48 -0400

    TST: add PytestTester from numpy

--- pywt/__init__.py
+++ pywt/__init__.py
@@ -35,5 +35,6 @@ except NameError:
 
 from pywt.version import version as __version__
 
-from numpy.testing import Tester
-test = Tester().test
+from ._pytesttester import PytestTester
+test = PytestTester(__name__)
+del PytestTester
