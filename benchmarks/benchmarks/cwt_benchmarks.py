fb4b030f4a06c63bebfdf10075538c222518bcfa
Gregory Lee <grlee77@gmail.com>
2019-07-26 01:40:34 -0400

    add float32 cases to cwt benchmarks

--- benchmarks/benchmarks/cwt_benchmarks.py
+++ benchmarks/benchmarks/cwt_benchmarks.py
@@ -9,20 +9,22 @@ class CwtTimeSuiteBase(object):
     params = ([32, 128, 512, 2048],
               ['cmor', 'cgau4', 'fbsp', 'gaus4', 'mexh', 'morl', 'shan'],
               [16, 64, 256],
-              ['conv', 'fft'])
-    param_names = ('n', 'wavelet', 'max_scale', 'method')
+              [np.float32, np.float64],
+              ['conv', 'fft'],
+              )
+    param_names = ('n', 'wavelet', 'max_scale', 'dtype', 'method')
 
-    def setup(self, n, wavelet, max_scale, method):
+    def setup(self, n, wavelet, max_scale, dtype, method):
         try:
             from pywt import cwt
         except ImportError:
             raise NotImplementedError("cwt not available")
-        self.data = np.ones(n, dtype='float')
-        self.scales = np.arange(1, max_scale+1)
+        self.data = np.ones(n, dtype=dtype)
+        self.scales = np.arange(1, max_scale + 1)
 
 
 class CwtTimeSuite(CwtTimeSuiteBase):
-    def time_cwt(self, n, wavelet, max_scale, method):
+    def time_cwt(self, n, wavelet, max_scale, dtype, method):
         try:
             pywt.cwt(self.data, self.scales, wavelet, method=method)
         except TypeError:
