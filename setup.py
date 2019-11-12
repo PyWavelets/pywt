2eca0900bbec15a41bc6bff3918677e497c2702c
Gregory R. Lee <gregory.lee@cchmc.org>
2019-11-12 13:43:28 -0500

    bump version and reset ISRELEASED to False

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ from setuptools import setup, Extension
 from setuptools.command.test import test as TestCommand
 
 MAJOR = 1
-MINOR = 1
+MINOR = 2
 MICRO = 0
-ISRELEASED = True
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
