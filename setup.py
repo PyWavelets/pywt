0cdeb7bd5fa3d2c1c2e780bc8ff3073a4be6c8d7
Gregory Lee <grlee77@gmail.com>
2020-11-07 02:36:53 -0500

    add Python 3.9 classifier

--- setup.py
+++ setup.py
@@ -425,6 +425,7 @@ def setup_package():
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Topic :: Software Development :: Libraries :: Python Modules"
         ],
         platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
