b121320c80ec5f45b86d2c9866043871507dea74
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-07-19 12:32:35 +0530

    STY: Fix style errors

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -343,7 +343,7 @@ Shall you encounter any issues, please feel free to report them on the
 [PyWavelets issue tracker](https://github.com/PyWavelets/pywt/issues)."""
 )
 
-jupyterlite_silence = True 
+jupyterlite_silence = True
 strip_tagged_cells = True
 
 # -- Options for MyST-NB and Markdown-based content --------------------------
