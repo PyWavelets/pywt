746d5690d5bc9fbbcf34dda2db12af3da17c5eca
Dang Thoai Phan <82931854+phandangthoai@users.noreply.github.com>
2025-08-05 11:45:22 +0200

    Update _cwt.py
    Fix doctest error

--- pywt/_cwt.py
+++ pywt/_cwt.py
@@ -114,7 +114,7 @@ def cwt(data, scales, wavelet, sampling_period=1., method='conv', axis=-1, preci
     >>> t = np.linspace(-1, 1, 200, endpoint=False)
     >>> sig  = np.cos(2 * np.pi * 7 * t) + np.real(np.exp(-7*(t-0.4)**2)*np.exp(1j*2*np.pi*2*(t-0.4)))
     >>> widths = np.logspace(np.log10(1), np.log10(30), 30)
-    >>> cwtmatr, freqs = pywt.cwt(sig, widths, 'mexh', , hop_size=128)
+    >>> cwtmatr, freqs = pywt.cwt(sig, widths, 'mexh', hop_size=128)
     >>> plt.imshow(cwtmatr, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
     ...            vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
     >>> plt.show()
