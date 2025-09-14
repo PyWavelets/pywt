e88ed4be079a8476d395e88615f42d5b4b0a8f44
Dang Thoai Phan <82931854+phandangthoai@users.noreply.github.com>
2025-09-14 12:43:28 +0200

    Update _cwt.py
    Update examples

--- pywt/_cwt.py
+++ pywt/_cwt.py
@@ -117,6 +117,29 @@ def cwt(data, scales, wavelet, sampling_period=1., method='conv', axis=-1, *, pr
     >>> plt.imshow(cwtmatr, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
     ...            vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
     >>> plt.show()
+
+    >>> import pywt
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> t = np.linspace(-1, 1, 200, endpoint=False)
+    >>> sig  = np.cos(2 * np.pi * 7 * t) + np.real(np.exp(-7*(t-0.4)**2)*np.exp(1j*2*np.pi*2*(t-0.4)))
+    >>> widths = np.logspace(np.log10(1), np.log10(30), 30)
+    >>> cwtmatr, freqs = pywt.cwt(sig, widths, 'mexh')
+    >>> hop_size = 16
+    >>> cwtmatr_h, freqs_h = pywt.cwt(sig, widths, 'mexh', hop_size=hop_size)
+    >>> fig, axes = plt.subplots(1, 2, figsize=(12, 5), sharey=True)
+    >>> im0 = axes[0].imshow(
+        cwtmatr, extent=[t[0], t[-1], widths[-1], widths[0]],
+        cmap='PRGn', aspect='auto',
+        vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max()
+    )
+    >>> axes[0].set_title("Original scalogram")
+    >>> im1 = axes[1].imshow(
+        cwtmatr_h, extent=[t[0], t[-1], widths[-1], widths[0]],
+        cmap='PRGn', aspect='auto',
+        vmax=abs(cwtmatr_h).max(), vmin=-abs(cwtmatr_h).max()
+    )
+    >>> axes[1].set_title("Scalogram with hop size")
     """
 
     # accept array_like input; make a copy to ensure a contiguous array
